e774c7b307a01d469af4bfdbd16b69a08be0b5f1
RJ Wright <rj@rjwright.me>
2023-04-13 17:15:49 +1000

    Create AddFishForm component to handle submitting new items to inventory.

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AddFishForm from "./AddFishForm";
 
 class Inventory extends React.Component {
   render() {
     return (
-      <div>
+      <div className="inventory">
         <h2>Inventory</h2>
+        <AddFishForm />
       </div>
     );
   }
