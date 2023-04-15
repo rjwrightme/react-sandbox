aeeb080ee9ebae4640a0ca7db3ac397c12ec2dbd
RJ Wright <rj@rjwright.me>
2023-04-15 16:16:53 +1000

    Update gitignore to exclude .env file. Set up Firebase with app for persistant storage of data.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,11 +4,22 @@ import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
 import Fish from "./Fish";
+import base from "../base";
 class App extends React.Component {
   state = {
     fishes: {},
     order: {},
   };
+  componentDidMount() {
+    this.ref = base.syncState(`${this.props.match.params.storeId}/fishes`, {
+      context: this,
+      state: "fishes",
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
+  }
   addFish = (fish) => {
     // 1. Take a copy of the existing state
     const fishes = { ...this.state.fishes };
