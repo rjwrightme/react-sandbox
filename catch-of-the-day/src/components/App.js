7e9e5ae7d7e20ec3cfc28ed561fd926feee11f4a
RJ Wright <rj@rjwright.me>
2023-04-15 15:34:29 +1000

    Update Fish component to have working Add to Cart button. Update Order component to display current order information.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -17,8 +17,18 @@ class App extends React.Component {
     // 3. Set the updated fishes object as the new state
     this.setState({ fishes });
   };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
+  };
+
+  addToOrder = (key) => {
+    // 1. Take a copy of state
+    const order = { ...this.state.order };
+    // 2. Add 1 to quantity if already there, otherwise add 1 if it doesn't yet exist
+    order[key] = order[key] + 1 || 1;
+    // 3. Call setState to update our state
+    this.setState({ order });
   };
   render() {
     return (
@@ -28,11 +38,16 @@ class App extends React.Component {
           <ul className="fishes">
             {/* Object.keys will make an array of the key values in our fishes state object. */}
             {Object.keys(this.state.fishes).map((key) => (
-              <Fish key={key} details={this.state.fishes[key]} />
+              <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder}
+              />
             ))}
           </ul>
         </div>
-        <Order />
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory
           addFish={this.addFish}
           loadSampleFishes={this.loadSampleFishes}
